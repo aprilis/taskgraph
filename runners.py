867704a3ad6c2cc2f2ff8d978191a42698c6ee6f
Jarosław Kwiecień <kwiecienjaro@gmail.com>
2019-10-15 21:44:04 +0200

    jupyter notebook runner

--- runners.py
+++ runners.py
@@ -1,4 +1,5 @@
 import os
+import importlib
 
 class Bash:
     def match(self, cmd):
@@ -9,7 +10,30 @@ class Bash:
 
 class Python:
     def match(self, cmd):
-        return cmd.split()[0].endswith('.py')
+        parts = cmd.split()
+        return len(parts) > 0 and parts[0].endswith('.py')
     
     def run(self, cmd):
         return Bash().run('python ' + cmd)
+
+class Notebook:
+    def __init__(self):
+        global nbconvert, nbformat
+        nbconvert = importlib.import_module('nbconvert')
+        nbformat = importlib.import_module('nbformat')
+
+    def match(self, cmd):
+        return cmd.endswith('.ipynb')
+
+    def run(self, cmd):
+        with open(cmd) as f:
+            nb = nbformat.read(f, as_version=4)
+        ep = nbconvert.preprocessors.ExecutePreprocessor()
+        ok = True
+        try:
+            ep.preprocess(nb)
+        except nbconvert.CellExecutionError:
+            ok = False
+        with open(cmd, 'w') as f:
+            nbformat.write(nb, f)
+        return ok
