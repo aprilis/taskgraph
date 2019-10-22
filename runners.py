e3c40cc6298255f375523d64f5dfd21ee65b55ec
Jaroslaw Kwiecien <kwiecienjaro@gmail.com>
2019-10-22 11:45:17 +0000

    fix jupyter error handling

--- runners.py
+++ runners.py
@@ -1,6 +1,14 @@
 import subprocess
 import os
 import importlib
+import logging
+
+try:
+    import nbconvert
+    import nbformat
+    has_jupyter = True
+except ImportError:
+    has_jupyter = False
 
 class Bash:
     def match(self, cmd):
@@ -21,15 +29,13 @@ class Python:
         return Bash().run('python ' + cmd, env, path)
 
 class Notebook:
-    def __init__(self):
-        global nbconvert, nbformat
-        nbconvert = importlib.import_module('nbconvert')
-        nbformat = importlib.import_module('nbformat')
-
     def match(self, cmd):
         return cmd.endswith('.ipynb')
 
     def run(self, cmd, env, path):
+        if not has_jupyter:
+            logging.error('Jupyter nbconvert is not installed')
+            return False
         with open(os.path.join(path, cmd)) as f:
             nb = nbformat.read(f, as_version=4)
         ep = nbconvert.preprocessors.ExecutePreprocessor()
@@ -40,7 +46,8 @@ class Notebook:
                     'path': path,
                     'kernelspec': { 'env': env }
                 }})
-        except nbconvert.CellExecutionError:
+        except nbconvert.preprocessors.CellExecutionError as e:
+            logging.error(str(e))
             ok = False
         with open(cmd, 'w') as f:
             nbformat.write(nb, f)
